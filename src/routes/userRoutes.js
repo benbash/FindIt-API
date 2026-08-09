c596b186d82c6db09a83435cd2daba33685b53af
benbash <bashharuna1@gmail.com>
2026-08-09 10:43:44 +0100

    update userRoutes.js to include updateProfile route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import express from "express";
-import authenticate from "../middleware/authMiddleware.js";
-import { getProfile } from "../controllers/userController.js";
-import { updateProfile } from "../controllers/userController.js";
-
-const router = express.Router();
-
-// Protected Routes
-router.get("/profile", authenticate, getProfile);
-router.patch("/profile", authenticate, updateProfile);
-
-export default router;  
-=======
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 import { getProfile, updateProfile } from '../controllers/userController.js';
@@ -23,4 +9,3 @@ router.get('/profile', protect, getProfile);
 router.patch('/profile', protect, validateProfileUpdate, updateProfile);
 
 export default router;
->>>>>>> origin/master
